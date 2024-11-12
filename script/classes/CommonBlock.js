31528a2a32e67090798b8b7d6e87661bd454a079
Alexgefestion <Alexgefestion@gmail.com>
2024-11-13 00:35:48 +0300

    обновление графики

--- script/classes/CommonBlock.js
+++ script/classes/CommonBlock.js
@@ -28,15 +28,86 @@ class CommonBlock{
 
 	width = 100;
 	miniIcon = "";
+	icon = [
+		[1,0,0,48,48],
+		[0,0,-48,15,-25],
+		[0,0,-48,-15,-25]
+	];
 
 	static getMiniIcon(){
 		return this.miniIcon;
 	}
+
+	//     0
+	//     ^
+	//     |
+	//3<---0--->1   направление которое задает квадрант
+	//     |        координаты в icon задаются отнотиельно центра изображения
+	//     v        нулевой индекс массива задает режим 0-линия(1инд=x1, 2инд=y1, 3инд=x2, 4инд=y2), 1-эллипс (1инд=cx, 2инд=cy, 3инд=rx, 4инд=ry)
+	//     2
+	rotateCordX(x,y,quadrant){
+		let cos = [0,1,0,-1];
+		let sin = [1,0,-1,0];
+		return x * sin[quadrant] - y * cos[quadrant];
+	}
+	rotateCordY(x,y,quadrant){
+		let cos = [0,1,0,-1];
+		let sin = [1,0,-1,0];
+		return x * cos[quadrant] + y * sin[quadrant];
+	}
+	iconRotatePrcessor(iconObjet, quadrant){
+		let cos = [0,1,0,-1];
+		let sin = [1,0,-1,0];
+		let newIcon = [];
+		for(let i = 0; i < iconObjet.length; i++){
+			newIcon[i] = [];
+			newIcon[i][0] = iconObjet[i][0];
+			newIcon[i][1] = this.rotateCordX(iconObjet[i][1], iconObjet[i][2], quadrant);
+			newIcon[i][2] = this.rotateCordY(iconObjet[i][1], iconObjet[i][2], quadrant);
+			newIcon[i][3] = this.rotateCordX(iconObjet[i][3], iconObjet[i][4], quadrant);
+			newIcon[i][4] = this.rotateCordY(iconObjet[i][3], iconObjet[i][4], quadrant);
+			if(iconObjet[i][0] == 1){
+				newIcon[i][3] = Math.abs(newIcon[i][3]);
+				newIcon[i][4] = Math.abs(newIcon[i][4]);			
+			}
+		}
+		return newIcon;
+	}
+	iconMarginPrcessor(iconObjet){
+		let newIcon = [];
+		for(let i = 0; i < iconObjet.length; i++){
+			newIcon[i] = [];
+			newIcon[i][0] = iconObjet[i][0];
+			newIcon[i][1] = iconObjet[i][1]+this.Xcord;
+			newIcon[i][2] = iconObjet[i][2]+this.Ycord;
+			if(iconObjet[i][0] == 0){
+				newIcon[i][3] = iconObjet[i][3]+this.Xcord;
+				newIcon[i][4] = iconObjet[i][4]+this.Ycord;			
+			}else{
+				newIcon[i][3] = iconObjet[i][3];
+				newIcon[i][4] = iconObjet[i][4];
+			}
+		}
+		return newIcon;
+	}
+	iconProcessor(iconObjet){
+		let newIcon = `<g id="block${this.id}" fill="${this.liquid.fill}" stroke="black" stroke-width="2px">`;
+		for(let i = 0; i < iconObjet.length; i++){
+			if(iconObjet[i][0] == 0){
+				newIcon += `<line x1="${iconObjet[i][1]}" y1="${iconObjet[i][2]}" x2="${iconObjet[i][3]}" y2="${iconObjet[i][4]}" />`		
+			}else{
+				newIcon += `<ellipse cx="${iconObjet[i][1]}" cy="${iconObjet[i][2]}" rx="${iconObjet[i][3]}" ry="${iconObjet[i][4]}" />`;
+			}
+		}
+		console.log(newIcon)
+		return newIcon;
+		// return `
+		// <svg id="block${this.id}" width="${this.width}" onmousedown="setCurrTarget(${this.id});setMode(0)" x="${this.Xcord}" y="${this.Ycord}" fill="${this.liquid.fill}"  stroke-width="2" stroke="black" width="100" height="100" viewBox="0 0 100 100">
+		// 	<circle id="block${this.id}" cx="50" cy="50" r="48"/>
+		// </svg> `
+	}
 	getIcon(){
-		return `
-		<svg id="block${this.id}" width="${this.width}" onmousedown="setCurrTarget(${this.id});setMode(0)" x="${this.Xcord}" y="${this.Ycord}" fill="${this.liquid.fill}"  stroke-width="2" stroke="black" width="100" height="100" viewBox="0 0 100 100">
-			<circle id="block${this.id}" cx="50" cy="50" r="48"/>
-		</svg> `
+		return this.iconProcessor(this.iconMarginPrcessor(this.iconRotatePrcessor(this.icon, 1)));
 	}
 
 	getOption(){
--- script/classes/Nasos1.js
+++ script/classes/Nasos1.js
@@ -1,30 +1,30 @@
 class Nasos1 extends CommonBlock{
 	name = "Насос"
-	getIcon(){
-		return `
-	<svg id="block${this.id}" width="${this.width}" x="${this.Xcord}" y="${this.Ycord}" fill="${this.liquid.fill}"  stroke-width="2" stroke="black" width="100" height="100" viewBox="0 0 100 100">
-		<circle id="block${this.id}" cx="50" cy="50" r="48"/>
-		<path fill="#000" d="M50,2 l15,20 l-30,0 l15,-20"/>
-	</svg> `
-	}
+	// getIcon(){
+	// 	return `
+	// <svg id="block${this.id}" width="${this.width}" x="${this.Xcord}" y="${this.Ycord}" fill="${this.liquid.fill}"  stroke-width="2" stroke="black" width="100" height="100" viewBox="0 0 100 100">
+	// 	<circle id="block${this.id}" cx="50" cy="50" r="48"/>
+	// 	<path fill="#000" d="M50,2 l15,20 l-30,0 l15,-20"/>
+	// </svg> `
+	// }
 	static miniIcon = 
 	`
 	<svg id="Nasos1" onClick="createBlock('Nasos1')" viewBox="0 0 100 100">
 		<circle cx="50" cy="50" r="48" fill="none" stroke-width="1" stroke="#dadaff"/>
-		<path d="M50,2 l15,20 l-30,0 l-15,20 "  fill="#dadaff"/>
+		<path d="M50,2 l15,20 l-30,0 l-15,20 " fill="#dadaff"/>
 	</svg>
 	`
 	nodes = [
 	{
-		cx:50,
-		cy:2,
+		cx:0,
+		cy:-48,
 		connected: -1,
 		diametr: 50,
 		roughness: 0.1
 	},
 	{
-		cx:50,
-		cy:98,
+		cx:0,
+		cy:48,
 		connected: -1,
 		diametr: 50,
 		roughness: 0.1
@@ -34,7 +34,7 @@ class Nasos1 extends CommonBlock{
 		let thisElem = workSpace.getElementById(`block${this.id}`);
 		for(let i = 0; i < this.nodes.length; i++){
 			thisElem.innerHTML+= `
-			<circle class="node" id="node${this.id}_${i}" onmousedown="pipe([${this.id},${i}])" cx="${this.nodes[i]['cx']}" cy="${this.nodes[i]['cy']}" r="5" fill="black" stroke-width="1" stroke="#dadaff"/>
+			<circle class="node" id="node${this.id}_${i}" onmousedown="pipe([${this.id},${i}])" cx="${this.nodes[i]['cx']+this.Xcord}" cy="${this.nodes[i]['cy']+this.Ycord}" r="5" fill="black" stroke-width="1" stroke="#dadaff"/>
 			`
 		}
 	}
